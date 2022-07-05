16ec14283ad796fb22fe29810bb3742555fac104
yinjiacheng <yinjiacheng@meituan.com>
2022-07-05 18:43:53 +0800

    id:HA-0000 cause:test solution:done

--- MyApplicationSTOkHttp/app/src/main/java/com/example/myapplicationstokhttp/MainActivity.java
+++ MyApplicationSTOkHttp/app/src/main/java/com/example/myapplicationstokhttp/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.myapplicationstokhttp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +21,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getDataSync();
+    }
+    private void getDataAsync() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://www.baidu.com")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                Log.d("MainActivity", response.body().string());
+                if(response.isSuccessful()){//回调的方法执行在子线程。
+                    Log.d("yjc","获取数据成功了");
+                    Log.d("yjc","response.code()=="+response.code());
+                    Log.d("yjc","response.body().string()=="+response.body().string());
+                }
+            }
+        });
     }
 
-    public void getDatasync(){
+
+    private void postDataWithParame() {
+        OkHttpClient client = new OkHttpClient();//创建OkHttpClient对象。
+        FormBody.Builder formBody = new FormBody.Builder();//创建表单请求体
+        formBody.add("username","zhangsan");//传递键值对参数
+        Request request = new Request.Builder()//创建Request对象。
+                .url("http://www.baidu.com")
+                .post(formBody.build())//传递请求体
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                Log.d("MainActivity", response.body().string());
+                if(response.isSuccessful()){//回调的方法执行在子线程。
+                    Log.d("yjc","获取数据成功了");
+                    Log.d("yjc","response.code()=="+response.code());
+                    Log.d("yjc","response.body().string()=="+response.body().string());
+                }
+            }
+        });//回调方法的使用与get异步请求相同，此时略。
+    }
+
+    public void getDataSync(){
         new Thread(new Runnable() {
             @Override
             public void run() {
