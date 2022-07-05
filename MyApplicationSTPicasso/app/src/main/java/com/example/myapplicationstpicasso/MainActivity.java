8c423d8f7bfc4d72283f60747b2fae5b9f48826f
yinjiacheng <yinjiacheng@meituan.com>
2022-07-05 19:53:29 +0800

    id:HA-0000 cause:test solution:done

--- MyApplicationSTPicasso/app/src/main/java/com/example/myapplicationstpicasso/MainActivity.java
+++ MyApplicationSTPicasso/app/src/main/java/com/example/myapplicationstpicasso/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplicationstpicasso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt = findViewById(R.id.button);
+        ImageView iv = findViewById(R.id.imageView);
+        bt.setOnClickListener(v -> {
+            Picasso.with(MainActivity.this).load("https://w.wallhaven.cc/full/y8/wallhaven-y8622k.jpg").into(iv);
+        });
+
     }
 }
